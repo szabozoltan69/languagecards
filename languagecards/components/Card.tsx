bcef647bcb6c6b7a2cd20184fbd2a2edb2184682
Szabo Zoltan <szabozoltan69@gmail.com>
2024-08-29 16:22:27 +0200

    Show solution nicely

--- languagecards/components/Card.tsx
+++ languagecards/components/Card.tsx
@@ -7,19 +7,20 @@ function Card(props_) {
   const [position, setPosition] = useState(0);
   const [removed, setRemoved] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
+  const [isKlicked, setIsKlicked] = useState(false);
   const p = 600; // position change
   const t = 50; // flash time for the solution before swiping
   const T = 500; // travel time
 
   const swipeHandler = useSwipe({
     onSwipedLeft: () => {
-      setIsClicked(true);
+      setIsKlicked(true);
       setTimeout(() => {setPosition(position - p)}, t);
       setTimeout(() => {setRemoved(true)}, T);
       setTimeout(() => {handleChange(props.id, false)}, T);
     },
     onSwipedRight: () => {
-      setIsClicked(true);
+      setIsKlicked(true);
       setTimeout(() => {setPosition(position + p)}, t);
       setTimeout(() => {setRemoved(true)}, T);
       setTimeout(() => {handleChange(props.id, true)}, T);
@@ -41,12 +42,15 @@ function Card(props_) {
 // <div className="sheetUrl">{props.id}</div>
   return (
     removed && hideMe ? null :
+    <>
+    {isKlicked && <div className="noHeight">{ mode === "foreign" ? props.text1 : props.text2}</div>}
     <div {...swipeHandler} onClick={handleClick} style={divStyle as React.CSSProperties}>
         { mode === "foreign" ?
           <div className="verticalSpace">{props.text2} {isClicked && <div>– {props.text1}<br/><i>{props.pronunciation}</i>{!!props.grammars.length ? <><br/><i>{props.grammars[0].grammar}</i></> : ""}</div>}</div> :
           <div className="verticalSpace">{props.text1} {isClicked && <div>– {props.text2}<br/><i>{props.pronunciation}</i>{!!props.grammars.length ? <><br/><i>{props.grammars[0].grammar}</i></> : ""}</div>}</div> }
     <br/>
     </div>
+    </>
   );
 }
 
