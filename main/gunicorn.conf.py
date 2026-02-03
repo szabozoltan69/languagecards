5931aae8c0e10ce5b2540239b16ea5332248d29c
Szabo Zoltan <szabozoltan69@gmail.com>
2026-02-03 15:06:23 +0100

    Nicer settings and env variables

--- main/gunicorn.conf.py
+++ main/gunicorn.conf.py
@@ -1,4 +1,5 @@
-bind = "0.0.0.0:8003" # can not be 80 as normal user
+import os
+bind = os.getenv("GUNICORN", "0.0.0.0:8003")  # can not be 80 as normal user
 workers = 3 # Adjust the number of workers as needed
 name = "main"
 timeout = 120
